e65e1230d2a98a3ce039a2521a55cafb669b3900
EliasOPrado <eliaspradoprofessional@outlook.com>
2019-07-31 10:21:26 +0100

    db relationship description

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    #Post db relationship:
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
@@ -18,6 +19,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    #User db relationship:
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
