ccfa9af55fd334b239b07a7d9269aa21be683f17
EliasOPrado <eliaspradoprofessional@outlook.com>
2019-08-01 16:03:10 +0100

    added upgrade to forms

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+    #Has to be equal to password above
+    password2 = PasswordField('Repeat Password', validators=[Datarequired(), EqualTo('password')])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+            
+    def validade_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user in not None:
+            raise ValidationError('Please use a different email address.')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request
 from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
+
 
 @app.route('/')
 @app.route('/index')
@@ -19,7 +21,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,8 +34,11 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+        
 
 @app.route('/logout')
 def logout():
