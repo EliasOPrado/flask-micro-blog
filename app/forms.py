c918172e1f06894cdad4c0de7d4d2084e276023b
EliasOPrado <eliaspradoprofessional@outlook.com>
2019-08-01 16:26:04 +0100

    added registration form function

--- app/forms.py
+++ app/forms.py
@@ -7,10 +7,18 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    #Has to be equal to password above
-    password2 = PasswordField('Repeat Password', validators=[Datarequired(), EqualTo('password')])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+    
+    
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    #Has to be equal to password above
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
     
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
@@ -19,5 +27,5 @@ class LoginForm(FlaskForm):
             
     def validade_email(self, email):
         user = User.query.filter_by(email=email.data).first()
-        if user in not None:
+        if user is not None:
             raise ValidationError('Please use a different email address.')
